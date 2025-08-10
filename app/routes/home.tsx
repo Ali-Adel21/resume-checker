a024637620c9468c3ed224a5e27d605c39b60b6d
Ali-Adel21 <aliasharaf474@gmail.com>
2025-08-11 01:04:08 +0300

    auth

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,48 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import type { Route } from "./+types/home" ;
+import ResumeCard from "~/components/ResumeCard";
+import { resumes } from "../../constants";
+import { useEffect } from "react";
+import {  useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ResumeRader" },
+    { name: "description", content: "Perfect your resume with intelligence" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+   const { isLoading, auth } = usePuterStore();
+    const navigate = useNavigate();
+
+    
+    useEffect(() => {
+        if (!auth.isAuthenticated) navigate('/auth?next=/');
+    }, [auth.isAuthenticated]);
+  
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    
+    <section className="main-section">
+        <div className="page-heading py-16">
+            <h1>Track Your Application Journey</h1>
+            <h2>Empowering job seekers with AI-driven resume analysis for better hiring outcomes.</h2>
+        </div>
+
+  {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+    </section>
+
+
+    
+
+
+  </main>
 }
